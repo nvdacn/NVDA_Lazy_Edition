afa895d9c865e3f5f6a9764651781e8eeb197f9b
WMHN1872265132 <1872265132@qq.com>
2024-03-27 09:02:23 +0800

    修正压缩包文件名的日期变量

--- 执行脚本.bat
+++ 执行脚本.bat
@@ -10,15 +10,19 @@ IF EXIST "%~dp0Temp" (rd /s /q "%~dp0Temp")
 
 Rem 创建便携版 NVDA
 "%~dp0Resource\%nvda%.exe" --create-portable-silent --portable-path="%~dp0Temp\NVDA"
+
+
+Rem 运行 NVDA（不含 GitHub Actions），设置版本日期变量
 if "%1" == "GITHUB_ACTIONS" (
-    Rem 在 GitHub Actions 中无需运行 NVDA
     @echo on
+set VersionDate=2024.03.27
 ) else (
     if /i %PROCESSOR_IDENTIFIER:~0,3%==x86 (
     Start /D  "%ProgramFiles%\NVDA" NVDA
     ) else (
     Start /D  "%ProgramFiles(x86)%\NVDA" NVDA
     )
+set VersionDate=%date:~3,4%.%date:~8,2%.%date:~11,2%
 )
 
 Rem 开始生成
@@ -28,7 +32,7 @@ COPY /B /V  /Y "%~dp0documentation\ReadMe.md" "%~dp0Output\说明.txt"
 "%~dp0Tools\InnoSetup\Compil32" /cc "%~dp0便携版安装脚本.iss"
 "%~dp0Tools\InnoSetup\Compil32" /cc "%~dp0懒人版安装脚本.iss"
 "%~dp0Tools\InnoSetup\Compil32" /cc "%~dp0恢复备份的 NVDA 配置.iss"
-"%~dp0Tools\7Zip\7z.exe" a -y -tzip "%~dp0Output\Archive\NVDA_Lazy_Edition_%date:~3,4%.%date:~8,2%.%date:~11,2%.zip" "%~dp0Output\NVDA 懒人版.exe" "%~dp0Output\更新日志.txt" "%~dp0Output\说明.txt" "%~dp0Output\NVDA 配置恢复工具.exe"
-"%~dp0Tools\7Zip\7z.exe" a -y -tzip "%~dp0Output\Archive\Source_Code_And_Dependency_Files_%date:~3,4%.%date:~8,2%.%date:~11,2%.zip" "%~dp0documentation" "%~dp0Resource" "%~dp0Tools" "%~dp0userConfig" "%~dp0ReadMe.md" "%~dp0便携版安装脚本.iss" "%~dp0恢复备份的 NVDA 配置.iss" "%~dp0懒人版安装脚本.iss" "%~dp0执行脚本.bat"
+"%~dp0Tools\7Zip\7z.exe" a -y -tzip "%~dp0Output\Archive\NVDA_Lazy_Edition_%VersionDate%.zip" "%~dp0Output\NVDA 懒人版.exe" "%~dp0Output\更新日志.txt" "%~dp0Output\说明.txt" "%~dp0Output\NVDA 配置恢复工具.exe"
+"%~dp0Tools\7Zip\7z.exe" a -y -tzip "%~dp0Output\Archive\Source_Code_And_Dependency_Files_%VersionDate%.zip" "%~dp0documentation" "%~dp0Resource" "%~dp0Tools" "%~dp0userConfig" "%~dp0ReadMe.md" "%~dp0便携版安装脚本.iss" "%~dp0恢复备份的 NVDA 配置.iss" "%~dp0懒人版安装脚本.iss" "%~dp0执行脚本.bat"
 
 Exit
