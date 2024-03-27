ff3722baaae6077e83d9df6ac6937bdfd4a3dad3
WMHN <1872265132@qq.com>
2024-03-27 19:39:06 +0800

    小修复 (#10)
    修改中文文档链接
    为首页文档添加指向更新日志的链接
    将 GitHub Actions构建流程与本地构建流程拆分
    修改 ISCC 参数，只输出错误消息
    删除PCQQ增强插件的描述字段

--- 执行脚本.bat
+++ 执行脚本.bat
@@ -11,26 +11,44 @@ IF EXIST "%~dp0Temp" (rd /s /q "%~dp0Temp")
 Rem 创建便携版 NVDA
 "%~dp0Resource\%nvda%.exe" --create-portable-silent --portable-path="%~dp0Temp\NVDA"
 
-Rem 运行 NVDA（不含 GitHub Actions），设置版本日期变量
-if "%1" == "GITHUB_ACTIONS" (
-  @echo on
-  set VersionDate=%date:~-4%.%date:~-10,2%.%date:~-7,2%
-) else (
-  if /i %PROCESSOR_IDENTIFIER:~0,3%==x86 (
-    Start /D  "%ProgramFiles%\NVDA" NVDA
-  ) else (
-    Start /D  "%ProgramFiles(x86)%\NVDA" NVDA
-  )
-  set VersionDate=%date:~3,4%.%date:~8,2%.%date:~11,2%
-)
-
-Rem 开始生成
+Rem 生成文档的 txt 版本
 MKDir "%~dp0Output"
 COPY /B /V  /Y "%~dp0documentation\changes.md" "%~dp0Output\更新日志.txt"
 COPY /B /V  /Y "%~dp0documentation\ReadMe.md" "%~dp0Output\说明.txt"
-"%~dp0Tools\InnoSetup\ISCC" /Qp "%~dp0便携版安装脚本.iss"
-"%~dp0Tools\InnoSetup\ISCC" /Qp "%~dp0懒人版安装脚本.iss"
-"%~dp0Tools\InnoSetup\ISCC" /Qp "%~dp0恢复备份的 NVDA 配置.iss"
+
+if "%1" == "GITHUB_ACTIONS" (
+  @echo on
+  GOTO GitHub
+) else (
+  GOTO Local
+)
+
+Rem GitHub Actions 构建流程
+:GitHub
+Rem 开始生成
+  set VersionDate=%date:~-4%.%date:~-10,2%.%date:~-7,2%
+"%~dp0Tools\InnoSetup\ISCC" /Q "%~dp0便携版安装脚本.iss"
+"%~dp0Tools\InnoSetup\ISCC" /Q "%~dp0懒人版安装脚本.iss"
+"%~dp0Tools\InnoSetup\ISCC" /Q "%~dp0恢复备份的 NVDA 配置.iss"
+GOTO Archive
+
+Rem 简体中文操作系统构建流程
+:Local
+Rem 运行 NVDA
+if /i %PROCESSOR_IDENTIFIER:~0,3%==x86 (
+  Start /D  "%ProgramFiles%\NVDA" NVDA
+) else (
+  Start /D  "%ProgramFiles(x86)%\NVDA" NVDA
+)
+
+Rem 开始生成
+set VersionDate=%date:~3,4%.%date:~8,2%.%date:~11,2%
+"%~dp0Tools\InnoSetup\Compil32" /cc "%~dp0便携版安装脚本.iss"
+"%~dp0Tools\InnoSetup\Compil32" /cc "%~dp0懒人版安装脚本.iss"
+"%~dp0Tools\InnoSetup\Compil32" /cc "%~dp0恢复备份的 NVDA 配置.iss"
+
+:Archive
+Rem 创建压缩包
 "%~dp0Tools\7Zip\7z.exe" a -y -tzip "%~dp0Output\Archive\NVDA_Lazy_Edition_%VersionDate%.zip" "%~dp0Output\NVDA 懒人版.exe" "%~dp0Output\更新日志.txt" "%~dp0Output\说明.txt" "%~dp0Output\NVDA 配置恢复工具.exe"
 "%~dp0Tools\7Zip\7z.exe" a -y -tzip "%~dp0Output\Archive\Source_Code_And_Dependency_Files_%VersionDate%.zip" "%~dp0documentation" "%~dp0Resource" "%~dp0Tools" "%~dp0userConfig" "%~dp0ReadMe.md" "%~dp0便携版安装脚本.iss" "%~dp0恢复备份的 NVDA 配置.iss" "%~dp0懒人版安装脚本.iss" "%~dp0执行脚本.bat"
 
