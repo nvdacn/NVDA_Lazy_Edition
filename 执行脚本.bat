d6d2746c4f0d251ffe0af3280208efdc0442aa0f
WMHN <1872265132@qq.com>
2024-03-31 13:54:46 +0800

    更新执行脚本 (#19)
    删除AISound5插件的aisound.dll
    为创建便携版NVDA的程序路径添加引号
    修复7Zip显示乱码

--- 执行脚本.bat
+++ 执行脚本.bat
@@ -7,7 +7,7 @@ IF EXIST "%~dp0Temp" (rd /s /q "%~dp0Temp")
 
 Rem 创建便携版 NVDA
 for /r "%~dp0Resource" %%i in (nvda_20*.exe) do (
-  %%i --create-portable-silent --portable-path="%~dp0Temp\NVDA"
+  "%%i" --create-portable-silent --portable-path="%~dp0Temp\NVDA"
 )
 
 Rem 生成文档的 txt 版本
@@ -24,6 +24,11 @@ if "%1" == "GITHUB_ACTIONS" (
 
 Rem GitHub Actions 构建流程
 :GitHub
+Rem 删除aisound.dll
+for /r "%~dp0Resource\Addons" %%i in (AISound*.nvda-addon) do (
+  "%~dp0Tools\7Zip\7z.exe" d -sccUTF-8 -y "%%i" "synthDrivers\aisound.dll"
+)
+
 Rem 开始生成
 set VersionDate=%date:~-4%.%date:~-10,2%.%date:~-7,2%
 "%~dp0Tools\InnoSetup\ISCC" /Q "%~dp0便携版安装脚本.iss"
@@ -48,7 +53,7 @@ set VersionDate=%date:~3,4%.%date:~8,2%.%date:~11,2%
 
 :Archive
 Rem 生成程序压缩文件
-"%~dp0Tools\7Zip\7z.exe" a -y -tzip "%~dp0Output\Archive\NVDA_Lazy_Edition_%VersionDate%.zip" "%~dp0Output\NVDA 懒人版.exe" "%~dp0Output\更新日志.txt" "%~dp0Output\说明.txt" "%~dp0Output\NVDA 配置恢复工具.exe"
-"%~dp0Tools\7Zip\7z.exe" a -y -tzip "%~dp0Output\Archive\Source_Code_And_Dependency_Files_%VersionDate%.zip" "%~dp0documentation" "%~dp0Resource" "%~dp0Tools" "%~dp0userConfig" "%~dp0ReadMe.md" "%~dp0便携版安装脚本.iss" "%~dp0恢复备份的 NVDA 配置.iss" "%~dp0懒人版安装脚本.iss" "%~dp0执行脚本.bat"
+"%~dp0Tools\7Zip\7z.exe" a -sccUTF-8 -y -tzip "%~dp0Output\Archive\NVDA_Lazy_Edition_%VersionDate%.zip" "%~dp0Output\NVDA 懒人版.exe" "%~dp0Output\更新日志.txt" "%~dp0Output\说明.txt" "%~dp0Output\NVDA 配置恢复工具.exe"
+"%~dp0Tools\7Zip\7z.exe" a -sccUTF-8 -y -tzip "%~dp0Output\Archive\Source_Code_And_Dependency_Files_%VersionDate%.zip" "%~dp0documentation" "%~dp0Resource" "%~dp0Tools" "%~dp0userConfig" "%~dp0ReadMe.md" "%~dp0便携版安装脚本.iss" "%~dp0恢复备份的 NVDA 配置.iss" "%~dp0懒人版安装脚本.iss" "%~dp0执行脚本.bat"
 
 Exit
